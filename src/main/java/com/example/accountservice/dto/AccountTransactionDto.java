c26f7b780bc33cc986b95ea8bf2ab903b2c0f72a
Mohsen Sabbaghi <m-abbaghi@agri-bank.com>
2022-06-07 16:56:54 +0430

    add account and Customer controllers

---
+++ src/main/java/com/example/accountservice/dto/AccountTransactionDto.java
@@ -0,0 +1,36 @@
+package com.example.accountservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author m-sabbaghi
+ * <a href="https://www.linkedin.com/in/sabbaghi/">...</a>
+ * @version 6/7/2022
+ */
+
+@Data
+@ToString(callSuper=true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AccountTransactionDto {
+
+    private Long id;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+
+    @NotNull(message = "can not be null")
+    private Long amount;
+
+    @JsonProperty("new_balance")
+    private Long newBalance;
+
+    @JsonProperty("reference_no")
+    private Long referenceNo;
+}
---
+++ src/main/java/com/example/accountservice/dto/CustomerDto.java
@@ -0,0 +1,40 @@
+package com.example.accountservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * @author m-sabbaghi
+ * <a href="https://www.linkedin.com/in/sabbaghi/">...</a>
+ * @version 6/7/2022
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomerDto {
+
+    private Long id;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+
+    @NotNull(message = "Name can not be null")
+    @Size(message = "name must be between 2 and 60 characters", min = 2, max = 50)
+    private String name;
+
+    @NotNull(message = "Surname can not be null")
+    @Size(message = "Surname must be between 2 and 50 characters", min = 2, max = 50)
+    private String surname;
+
+    private Set<AccountDto> accounts;
+}
